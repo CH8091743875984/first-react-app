a275e37db14a75c7e8b06459f7349bd76e5c79c2
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-01-22 20:31:45 -0600

    Added first test components

---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return <h1>This is my test heading for this React project.</h1>;
+}
+
+function FavoriteFood() {
+  return <h2>Pizza is my favorite food.</h2>;
+}
+
+export { Greeting, FavoriteFood };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Greeting, FavoriteFood } from "./Greeting.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Greeting />
+    <FavoriteFood />
+  </StrictMode>
+);
+
+//run npm run dev to launch the server
+//go to localhost:5173
